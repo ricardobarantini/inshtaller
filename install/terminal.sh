95a9a88481adbf38362ac7610bdff39430ec8546
Ricardo Barantini <ricardobarantini@gmail.com>
2025-06-30 11:19:21 -0300

    update oh-my-zsh install script

--- install/terminal.sh
+++ install/terminal.sh
@@ -2,7 +2,7 @@
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
 
 # Oh-My-Zsh
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Starship
 curl -sS https://starship.rs/install.sh | sh
